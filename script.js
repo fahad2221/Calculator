512f401d40d1a9b8ea5161e2e9f30d2ee1e23519
Fahad Mustafa <fahad-2221@hotmail.co.uk>
2020-05-29 15:04:59 +0100

    Finished functionality of calculator

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const input = document.querySelector('.keypad');
 const output = document.querySelector('.view-key');
 let clicks = 0;
 let runningTotal = 0;
-let buffer = "";
+let buffer = 0;
 let lastOperator = null;
 input.addEventListener('click', function () {
     //inital zeros
@@ -18,18 +18,68 @@ input.addEventListener('click', function () {
     }
     //addition
     if (event.target.className == "numberSquare topKey plus") {
-        runningTotal += pasrseInt(output.innerText);
+        buffer = parseInt(output.innerText);
         lastOperator = "+";
-        output.innerText = "";
+        output.innerText = "0";
+        clicks = 0;
+    }
+    //negate
+    if(event.target.className == "numberSquare botRow minus"){
+        buffer = parseInt(output.innerText);
+        clicks = 0;
+        output.innerText= "0";
+        lastOperator = "-";
+    }
+    //multiply
+    if(event.target.className == "numberSquare topKey multiply"){
+        buffer = parseInt(output.innerText);
+        clicks = 0;
+        output.innerText= "0";
+        lastOperator = "x";
+    }
+    //division
+    if(event.target.className == "numberSquare topKey divide"){
+        buffer = parseInt(output.innerText);
+        clicks = 0;
+        output.innerText = "0";
+        lastOperator = "/";
     }
     //clear button
     if (event.target.className == "numberSquare topRow big") {
         clicks = 0;
-        output.innerText = "0"
+        output.innerText = "0";
         lastOperator = null;
+        runningTotal = 0;
     }
-
     //equals button
-    if(event.target.className == "numberSquare topRow equals")
+    if(event.target.className == "numberSquare topKey equals"){
+        switch (lastOperator){
+            case '+':
+                let temp = parseInt(output.innerText) + buffer;
+                output.innerText = "" + temp;
+                runningTotal += temp;
+                buffer = 0;
+                break;
+            case '-':
+                let temp2 = buffer - parseInt(output.innerText);
+                output.innerText = "" + temp2;
+                runningTotal += temp2;
+                buffer = 0;
+                break;
+            case 'x':
+                let temp3 = buffer * parseInt(output.innerText);
+                output.innerText = "" + temp3;
+                runningTotal += temp3;
+                buffer = 0;
+                break;
+            case '/':
+                let temp4 = buffer / parseInt(output.innerText);
+                output.innerText = "" + temp4;
+                runningTotal += temp4;
+                buffer = 0;
+                break;  
+        }   
+
+    }
 
 });
